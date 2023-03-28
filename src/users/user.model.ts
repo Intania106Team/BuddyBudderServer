575e00475501c51b6ee70fc001bec98f2372a5c6
Thanapat Chotipun <66824385+PatrickChoDev@users.noreply.github.com>
2023-03-28 07:53:04 +0700

    Update user.model.ts

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -11,7 +11,7 @@ export class User {
   @Column({ type: 'varchar' })
   name: string;
 
-  @Column({ type: 'char' })
+  @Column({ type: 'varchar' })
   group: string;
 
   @Column({ type: 'varchar', nullable: true })
